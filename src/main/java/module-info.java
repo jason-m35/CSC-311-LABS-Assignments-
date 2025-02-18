3ac4ec2f8c303f0d1c444da6525e1146cff34981
jason-m35 <150742086+jason-m35@users.noreply.github.com>
2025-02-17 23:03:43 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.farmingdale.csc311_gui_basics {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.farmingdale.csc311_gui_basics to javafx.fxml;
-    exports edu.farmingdale.csc311_gui_basics;
-}
